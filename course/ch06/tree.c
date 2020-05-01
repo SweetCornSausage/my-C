da44a6489d79591d0624f0f3aa31f3a04d452eae
SweetCornSausage <zhenlei5ge@gmail.com>
2020-05-01 21:00:57 +0800

    enter

---
+++ course/ch06/tree.c
@@ -0,0 +1,85 @@
+//#include "queue_ar.h"
+#include <stdio.h>
+#include <stdlib.h>
+typedef int ElementType;
+struct Node;
+typedef struct Node *PtrToNode;
+typedef struct Node *Tree;
+
+Tree CreateTree();
+void CreateFromStdin(Tree T);
+
+void ScanPre(Tree T);
+void ScanIn(Tree T);
+void ScanBack(Tree T);
+
+struct Node
+{
+    ElementType data;
+    PtrToNode lchild;
+    PtrToNode rchild;
+};
+
+Tree CreateTree()
+{
+    Tree t = malloc(sizeof(struct Node));
+    return t;
+}
+
+void CreateFromStdin(Tree T)
+{
+    ElementType ch;
+    ch = getchar();
+    if (ch == '#')
+        T = NULL;
+    else
+    {
+        T->data = ch;
+        T->lchild = malloc(sizeof(struct Node));
+        T->rchild = malloc(sizeof(struct Node));
+        CreateFromStdin(T->lchild);
+        CreateFromStdin(T->rchild);
+    }
+}
+
+void ScanPre(Tree T)
+{
+    if (T->data != 0)
+    {
+        printf("%c", T->data);
+        ScanPre(T->lchild);
+        ScanPre(T->rchild);
+    }
+}
+
+void ScanIn(Tree T)
+{
+    if (T->data != 0)
+    {
+        ScanIn(T->lchild);
+        printf("%c", T->data);
+        ScanIn(T->rchild);
+    }
+}
+
+void ScanBack(Tree T)
+{
+    if (T->data != 0)
+    {
+        ScanBack(T->lchild);
+        ScanBack(T->rchild);
+        printf("%c", T->data);
+    }
+}
+
+int main()
+{
+    Tree t = CreateTree();
+    CreateFromStdin(t);
+    printf("先序输出:\n");
+    ScanPre(t);
+    printf("\n中序输出:\n");
+    ScanIn(t);
+    printf("\n后序输出:\n");
+    ScanBack(t);
+}
